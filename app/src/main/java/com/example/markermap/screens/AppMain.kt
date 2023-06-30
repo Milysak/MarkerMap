634a2f94dc2827b0c12a81084729f06648525b90
Miłosz Flasz <miloszflasz12345@gmail.com>
2023-06-30 18:47:18 +0200

    Add bottom navigation bar

--- app/src/main/java/com/example/markermap/screens/AppMain.kt
+++ app/src/main/java/com/example/markermap/screens/AppMain.kt
@@ -1,40 +1,121 @@
 package com.example.markermap.screens
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.bottomnavbardemo.screens.HomeScreen
+import com.example.bottomnavbardemo.screens.MapScreen
+import com.example.bottomnavbardemo.screens.SettingsScreen
 
-@Composable
-fun NavigationBar(
-    modifier: Modifier = Modifier,
-    containerColor: Color = NavigationBarDefaults.containerColor,
-    contentColor: Color = MaterialTheme.colorScheme.contentColorFor(containerColor),
-    tonalElevation: Dp = NavigationBarDefaults.Elevation,
-    windowInsets: androidx.compose.foundation.layout.WindowInsets = NavigationBarDefaults.windowInsets,
-    content: @Composable RowScope.() -> Unit
+sealed class BottomBarScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
 ) {
+    object Home : BottomBarScreen(
+        route = "home",
+        title = "Home",
+        icon = Icons.Default.Home
+    )
+
+    object Profile : BottomBarScreen(
+        route = "map",
+        title = "Map",
+        icon = Icons.Default.List
+    )
+
+    object Settings : BottomBarScreen(
+        route = "settings",
+        title = "Settings",
+        icon = Icons.Default.Settings
+    )
 }
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun AppMain(){
-        var selectedItem by remember { mutableStateOf(0) }
-        val items = listOf("Songs", "Artists", "Playlists")
+fun BottomNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarScreen.Home.route
+    ) {
+        composable(route = BottomBarScreen.Home.route) {
+            HomeScreen()
+        }
+        composable(route = BottomBarScreen.Profile.route) {
+            MapScreen()
+        }
+        composable(route = BottomBarScreen.Settings.route) {
+            SettingsScreen()
+        }
+    }
+}
 
-        NavigationBar {
-            items.forEachIndexed { index, item ->
-                NavigationBarItem(
-                    icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
-                    label = { Text(item) },
-                    selected = selectedItem == index,
-                    onClick = { selectedItem = index }
-                )
+@Composable
+fun AppMain() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomBar(navController = navController) }
+    ) {
+        BottomNavGraph(navController = navController)
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Profile,
+        BottomBarScreen.Settings,
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    NavigationBar {
+        screens.forEach { screen ->
+            AddItem(
+                screen = screen,
+                currentDestination = currentDestination,
+                navController = navController,
+            )
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    NavigationBarItem (
+        label = {
+            Text(text = screen.title)
+        },
+        icon = {
+            Icon(
+                imageVector = screen.icon,
+                contentDescription = "Navigation Icon"
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screen.route
+        } == true,
+        onClick = {
+            navController.navigate(screen.route) {
+                // Back button get to the findStartDestination - Home
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
             }
         }
+    )
 }
